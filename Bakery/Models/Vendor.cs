d9fd8e6d4371064ad6600eb9342d307f716e83e9
John Diggins <rohnfiggins@gmail.com>
2023-03-03 10:15:41 -0800

    v1 (vendor test 1) passed: Vendor constructor ok

--- Bakery/Models/Vendor.cs
+++ Bakery/Models/Vendor.cs
@@ -4,18 +4,18 @@ namespace Bakery.Models
 {
   public class Vendor
   {
-    public string Name { get ; set;}
-    private static List<Vendor> _instances = new List<Vendor> {};
+    public string Name { get; set; }
+    private static List<Vendor> _instances = new List<Vendor> { };
 
-    public Vendor(string Name)
+    public Vendor(string name)
     {
       Name = name;
       _instances.Add(this);
     }
 
-  public static void ClearAll()
-  {
-_instances.Clear();
-  }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
   }
 }
