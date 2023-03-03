343c18a3bceab976c053af10df8b9c449d9a0a6a
John Diggins <120290438+John-Dig@users.noreply.github.com>
2023-03-03 08:26:35 -0800

    Initial commit

---
+++ TEMPLATE.tests/ModelTests/TEMPLATETests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TEMPLATE.Models;
+
+namespace TEMPLATE.Tests
+{
+  [TestClass]
+  public class TEMPLATE
+  {
+    // Tests will go here!
+  }
+}
